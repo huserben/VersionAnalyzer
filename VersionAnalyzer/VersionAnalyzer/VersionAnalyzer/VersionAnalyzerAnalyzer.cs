ef9124b717a89b9cb8b24d95a10bc434f92ca493
Benjamin Huser <benjamin.huser@ch.abb.com>
2018-06-23 14:02:08 +0200

    Added actual anylzer and codefix implementation

--- VersionAnalyzer/VersionAnalyzer/VersionAnalyzer/VersionAnalyzerAnalyzer.cs
+++ VersionAnalyzer/VersionAnalyzer/VersionAnalyzer/VersionAnalyzerAnalyzer.cs
@@ -10,42 +10,44 @@ using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace VersionAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class VersionAnalyzerAnalyzer : DiagnosticAnalyzer
-    {
-        public const string DiagnosticId = "VersionAnalyzer";
+   [DiagnosticAnalyzer(LanguageNames.CSharp)]
+   public class VersionAnalyzerAnalyzer : DiagnosticAnalyzer
+   {
+      public const string DiagnosticId = "VersionAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+      // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+      // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+      private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+      private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+      private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+      private const string Category = "Naming";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+      private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+      public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
-        public override void Initialize(AnalysisContext context)
-        {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-        }
+      public override void Initialize(AnalysisContext context)
+      {
+         // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+         context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.ObjectCreationExpression);
+      }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+      private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+      {
+         var objectCreationExpressionSyntax = (ObjectCreationExpressionSyntax)context.Node;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+         if (objectCreationExpressionSyntax.Type is IdentifierNameSyntax identifierNameSyntax &&
+             identifierNameSyntax.Identifier.Text == "Version")
+         {
+            var argumentList = objectCreationExpressionSyntax.ArgumentList.Arguments;
+
+            if (argumentList.Count != 4 && argumentList.Count != 1)
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
+               var diagnostic = Diagnostic.Create(Rule, objectCreationExpressionSyntax.GetLocation());
+               context.ReportDiagnostic(diagnostic);
             }
-        }
-    }
+         }
+      }
+   }
 }
--- VersionAnalyzer/VersionAnalyzer/VersionAnalyzer/VersionAnalyzerCodeFixProvider.cs
+++ VersionAnalyzer/VersionAnalyzer/VersionAnalyzer/VersionAnalyzerCodeFixProvider.cs
@@ -15,59 +15,60 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace VersionAnalyzer
 {
-    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(VersionAnalyzerCodeFixProvider)), Shared]
-    public class VersionAnalyzerCodeFixProvider : CodeFixProvider
-    {
-        private const string title = "Make uppercase";
+   [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(VersionAnalyzerCodeFixProvider)), Shared]
+   public class VersionAnalyzerCodeFixProvider : CodeFixProvider
+   {
+      private const string title = "Add missing Digits";
 
-        public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(VersionAnalyzerAnalyzer.DiagnosticId); }
-        }
+      public sealed override ImmutableArray<string> FixableDiagnosticIds
+      {
+         get { return ImmutableArray.Create(VersionAnalyzerAnalyzer.DiagnosticId); }
+      }
 
-        public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+      public sealed override FixAllProvider GetFixAllProvider()
+      {
+         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+         return WellKnownFixAllProviders.BatchFixer;
+      }
 
-        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+      public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+      {
+         var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+         // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+         var diagnostic = context.Diagnostics.First();
+         var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+         // Find the type declaration identified by the diagnostic.
+         var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ObjectCreationExpressionSyntax>().First();
 
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c), 
-                    equivalenceKey: title),
-                diagnostic);
-        }
+         // Register a code action that will invoke the fix.
+         context.RegisterCodeFix(
+             CodeAction.Create(
+                 title: title,
+                 createChangedDocument: c => AddMissingDigits(context.Document, declaration, c),
+                 equivalenceKey: title),
+             diagnostic);
+      }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
-        {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+      private async Task<Document> AddMissingDigits(Document document, ObjectCreationExpressionSyntax objectCreationExpressionSyntax, CancellationToken cancellationToken)
+      {
+         var existingArgumentList = objectCreationExpressionSyntax.ArgumentList;
+         var newArgumentsList = SyntaxFactory.ArgumentList(existingArgumentList.OpenParenToken, existingArgumentList.Arguments, existingArgumentList.CloseParenToken);
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+         for (var argumentIndex = newArgumentsList.Arguments.Count; argumentIndex < 4; argumentIndex++)
+         {
+            var newArgument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                  SyntaxFactory.Literal(0)));
+            newArgumentsList = newArgumentsList.AddArguments(newArgument);
+         }
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+         var newObjectCreationExpressionSyntax = objectCreationExpressionSyntax.WithArgumentList(newArgumentsList);
+         var root = await document.GetSyntaxRootAsync();
 
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
-        }
-    }
+         var newRoot = root.ReplaceNode(objectCreationExpressionSyntax, newObjectCreationExpressionSyntax);
+
+         return document.WithSyntaxRoot(newRoot);
+      }
+   }
 }
